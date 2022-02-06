62e9c87a25ea1996d00a3c3e0e976d09ed1fdaf8
Andre Felipe <andre.felipe@novam3.com>
2022-02-06 19:35:47 -0300

    verificando palindromo

---
+++ verificarPalindromo.js
@@ -0,0 +1,21 @@
+const verificarPalindromo1 = (string)=> {
+    if(!string) return;
+    
+    return string.split("").reverse().join("") === string;
+}
+
+
+const verificarPalindromo2 = (string)=> {
+    if(!string) return;
+
+    for (let i = 0; i < string.lenght / 2; i++) {
+        if(string[i] !== string.lenght -1 - i) {
+            return false;
+        } 
+    }
+
+    return true;
+}
+
+
+console.log(verificarPalindromo2("ovo"));
